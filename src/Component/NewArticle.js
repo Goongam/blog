3db66b695d509abb9142dfdaaedfa731e1791336
Goongam <dlwjddn341@naver.com>
2022-09-22 23:48:55 +0900

    newArticle memo, useQuery

--- src/Component/NewArticle.js
+++ src/Component/NewArticle.js
@@ -4,6 +4,7 @@ import MenuItem from '@mui/material/MenuItem';
 import FormControl from '@mui/material/FormControl';
 import Select from '@mui/material/Select';
 import TextField from '@mui/material/TextField';
+import { useMutation } from "react-query";
 // import { Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle } from "@mui/material";
 
 function NewArticle({categoryList, addCategory}){
@@ -11,53 +12,32 @@ function NewArticle({categoryList, addCategory}){
     const [title,setTitle] =  useState("");
     const [content, setContent] = useState("");
     const [selectedCate, setSelectedCate] = useState("");
-    console.log('newArticle render');
-  //   const [dialogopen, setDialogOpen] = useState(false);
-  //   const [inputCategory, setinputCategory] = useState('');
 
-  //   function handleClose(){
-  //     setDialogOpen(false);
-  //   }
-  //   function DialogAdd(e){
-  //     if(inputCategory !== "") addCategory(inputCategory);
-  //     setDialogOpen(false);
-  //   }
-  //  function handleClickOpen(){
-  //     setDialogOpen(true);
-  //   }
-    // useEffect(()=>{
-    //   async function getCategory(){
-    //     const categoryList = await (await fetch("http://localhost:3001/GetCategoryList")).json();
-    //     setCategorys(categoryList);
-    //   }
-    //   getCategory();
-    // },[]);
+    const createArticle = useMutation(async ()=> await (await fetch("http://localhost:3001/NewArticle", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        "title":title,
+        "content":content,
+        "category":selectedCate,
+      }),
+      
+    })).json(),{
+      onSuccess: ()=>{console.log("생성성공")},
+      onError: ()=>{console.log('삭제실패')}
+    })
 
-    async function send(){
-        // console.log(title);
-        // console.log(content);
-
-       let json = await (await fetch("http://localhost:3001/NewArticle", {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json",
-            },
-            body: JSON.stringify({
-              "title":title,
-              "content":content,
-              "category":selectedCate,
-            }),
-            
-          })).json();
-
-        console.log(json);
-        // getArticles();
-    }
+    console.log(createArticle.data);
 
     return (
         <>
-            <input type="text" value={title} onChange={(e)=>{setTitle(e.target.value)}}></input><br></br>
-            <textarea value={content} onChange={(e)=>{setContent(e.target.value)}}></textarea><br></br>
+            {/*Memo를 사용한 input rerender방지*/}
+            {/* <input type="text" onChange={(e)=>{setTitle(e.target.value)}}></input><br></br> */}
+            {/* <textarea value={content} onChange={(e)=>{setContent(e.target.value)}}></textarea><br></br> */}
+            <InputTitleMemo setTitle={setTitle} title={title} />
+            <InputContentMemo setContent={setContent} content={content} />
             <FormControl fullWidth>
               <InputLabel id="demo-simple-select-label">Category</InputLabel>
               <Select
@@ -75,9 +55,27 @@ function NewArticle({categoryList, addCategory}){
 
               </Select>
             </FormControl>
-            <button onClick={send}>입력</button>
+            <button onClick={createArticle.mutate}>입력</button>
         </>
     );
+}
+
+const InputTitleMemo = memo(InputTitle);
+function InputTitle({setTitle, title}){
+  console.log('t render')
+  return (<>
+    <input type="text" value={title} onChange={(e)=>{setTitle(e.target.value)}}></input><br></br>
+  </>);
+}
+
+const InputContentMemo = memo(InputContent);
+console.log('c redere');
+function InputContent({setContent, content}){
+  return (
+    <>
+      <textarea value={content} onChange={(e)=>{setContent(e.target.value)}}></textarea><br></br>
+    </>
+  );
 }
 
 export default NewArticle;
